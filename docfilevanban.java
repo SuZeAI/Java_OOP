13f5aeae5300289e85a54fa15d55f1c982292a80
SuZeAI <goohaa1234@gmail.com>
2023-10-09 12:23:45 +0700

    JAVA

---
+++ docfilevanban.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.Math;
+import java.math.BigInteger;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class docfilevanban {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("DATA.in"));
+        while(sc.hasNextLine()){
+            System.out.println(sc.nextLine());
+        }
+    }   
+}
